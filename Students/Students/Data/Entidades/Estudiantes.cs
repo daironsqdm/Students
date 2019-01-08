4db8662265be2e933675aef88bf24048b9ad2a9f
Dairon Castro Casas <DaironCasas@sqdm.com>
2019-01-08 09:15:49 -0500

    ultimos cambios

---
+++ Students/Students/Data/Entidades/Estudiantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students.Data.Entidades
+{
+    public class Estudiantes
+    {
+        public int IdEstudiante { get; set; }
+        public string NombreEstudiante { get; set; }
+        public string ApellidoEstudiante { get; set; }
+        public string DocumentoEstudiante { get; set; }
+    }
+}
